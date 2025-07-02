cd608244bbf9803976ab9d589714e44664a80e10
FasterSt <eleiker1212mendez@gmail.com>
2025-07-02 00:19:59 -0500

    feat: change types and function createUser

--- database/models/user.ts
+++ database/models/user.ts
@@ -1,26 +1,23 @@
 import { DatabaseManager } from "@/database/base";
 
-export interface User {
+export interface UserDb {
     id: number;
     name: string;
     email: string;
-    password: string;
     createdAt: string;
     updatedAt: string;
 }
 
-class UsersDb {
-    private static instance: User;
+export type User = Omit<UserDb, 'id' | 'createdAt' | 'updatedAt'>;
 
-    public async createUser(): Promise<void> {
-        try {
-            const dbManager = DatabaseManager.getInstance();
-            const db = await dbManager.getDbInstance();
-            await db.runAsync('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [this.name, this.email, this.password]);
-            console.log("User created successfully");
-        } catch (error) {
-            console.error('Error creating user:', error);
-            throw error;
-        }
+export async function createUser({name, email, }: User): Promise<void> {
+    try {
+        const dbManager = DatabaseManager.getInstance();
+        const db = await dbManager.getDbInstance();
+        await db.runAsync('INSERT INTO users (name, email) VALUES (?, ?)', [name, email]);
+        console.log("User created successfully");
+    } catch (error) {
+        console.error('Error creating user:', error);
+        throw error;
     }
 }
